f40e413c5d1fdc470582daa530362acd7b5ceb86
Vicheak <suonvicheak991@gmail.com>
2024-06-17 12:43:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jenkins Build Successfully
+          Jenkins Deployed Successfully
         </p>
       </header>
     </div>
